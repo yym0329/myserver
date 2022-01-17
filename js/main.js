8b432a3fe7d0e0e30f0ecb60e88dbc3e26e53cff
yym0329 <yym3055@gmail.com>
2022-01-17 22:14:12 +0900

    signin project 시작, common.js, common.css로 의존성 정리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// 검색칸 토글 기능
-
-const searchEl = document.querySelector('.search'); // document 전체에서 .search 클래스를 갖는 첫번째 요소를 찾음.
-const searchInputEl = searchEl.querySelector('input');
-// searchEl 요소 안에서 input 클래스를 갖는 요소를 찾는다.
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 // 헤더 뱃지 스크롤 fade-in, fade-out 구현
 
 const badgeEl = document.querySelector('header .badges');
@@ -193,5 +173,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 올해 연도 반환
